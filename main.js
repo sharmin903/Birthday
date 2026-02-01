0dfeea69a157c80529f11a7c094dd8cdb6201280
Sharmin Akther <sharmin_cse@istt.edu.bd>
2026-02-02 00:41:22 +0600

    mouse control

--- main.js
+++ main.js
@@ -38,6 +38,40 @@ function handleTouchEnd(e){
     }
     isSwiping=false;
 }
+function handleMouseDown(e){
+    if(hasSwapped)return;
+    startX=e.clientX;
+    isSwiping=true;
+}
+function handleMouseMove(e){
+    if(!isSwiping || hasSwapped)return;
+    currentX=e.clientX;
+    const diff= startX-currentX;
+    if (diff>0){
+        cakeScreen.style.transform = `translateX(-${diff}px)`;
+        letterScreen.style.transform=`translateX(calc(100% - ${diff}px))`;
+    }
+}
+function handleMouseUp(e){
+    if(!isSwiping || hasSwapped) return;
+    const diff= startX - currentX;
+    if (diff>100){
+        hasSwapped=true;
+        cakeScreen.style.transform='';
+        letterScreen.style.transform='';
+        cakeScreen.classList.add('swiped');
+        letterScreen.classList.add('show');
+    }
+    else{
+  cakeScreen.style.transform='translateX(0)';
+        letterScreen.style.transform=`translateX(100%)`
+    }
+    isSwiping=false;
+}
 cakeScreen.addEventListener('touchstart', handleTouchStart);
 cakeScreen.addEventListener('touchmove', handleTouchMove);
 cakeScreen.addEventListener('touchend', handleTouchEnd);
+
+cakeScreen.addEventListener('mousedown',handleMouseDown);
+cakeScreen.addEventListener('mousemove',handleMouseMove);
+cakeScreen.addEventListener('mouseup',handleMouseUp);
